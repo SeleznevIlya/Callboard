202454a892b73a7abd93dde2d8f87c70e198ff1d
Helfik <helfik123_1998@mail.ru>
2023-01-10 22:45:07 +0300

    Начал прикручивать ckeditor. Далее шаблоны и вьюшки

--- Callboard_/Callboard_/settings.py
+++ Callboard_/Callboard_/settings.py
@@ -42,8 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'callboard_project'
+    'callboard_project',
+    'ckeditor',
+    'ckeditor_uploader'
 ]
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,6 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Callboard_/callboard_project/models.py
+++ Callboard_/callboard_project/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Category(models.Model):
@@ -19,7 +20,7 @@ class Subscribers(models.Model):
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     header = models.CharField(max_length=255)
-    content = models.TextField()
+    content = RichTextUploadingField()
     datetime = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
