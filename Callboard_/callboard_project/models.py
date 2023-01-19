bcafab41438d8fc45719bc3142cc19122c9be974
Helfik <helfik123_1998@mail.ru>
2023-01-19 21:07:13 +0300

    Всплыл баг с созданием поста, починил

--- Callboard_/callboard_project/models.py
+++ Callboard_/callboard_project/models.py
@@ -12,19 +12,19 @@ class VerifiedUser(models.Model):
 
 class Category(models.Model):
     category_name = models.CharField(max_length=255, unique=True)
-    subscriber = models.ManyToManyField(VerifiedUser, through='Subscribers')
+    subscriber = models.ManyToManyField(User, through='Subscribers')
 
     def __str__(self):
         return f'{self.category_name}'
 
 
 class Subscribers(models.Model):
-    user = models.ForeignKey(VerifiedUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Post(models.Model):
-    author = models.ForeignKey(VerifiedUser, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     header = models.CharField(max_length=255)
     content = RichTextUploadingField()
     datetime = models.DateTimeField(auto_now_add=True)
@@ -38,7 +38,7 @@ class Post(models.Model):
 
 
 class Reply(models.Model):
-    author = models.ForeignKey(VerifiedUser, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     datetime = models.DateTimeField(auto_now_add=True)
